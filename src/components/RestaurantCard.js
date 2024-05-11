5a23820e51d171eb56d7ae66996110865e6f6bc1
Gaurav Prasad <gaurav.prasad2305@outlook.com>
2024-05-11 18:21:30 +0530

    Refactored code and moved component into specific folder

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,23 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const { name, cuisines, avgRating } = resData.info;
+  const { deliveryTime } = resData.info.sla;
+  console.log(props);
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={CDN_URL + resData.info.cloudinaryImageId}
+      ></img>
+      <h4>{name}</h4>
+      <h5>{cuisines.join(", ")}</h5>
+      <h5>{avgRating} stars</h5>
+      <h5>In {deliveryTime} minutes</h5>
+    </div>
+  );
+};
+
+export default RestaurantCard;
